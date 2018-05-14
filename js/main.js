28083622848fa643892d77dd4fee47c4e6b998a0
Lisa Trevis <l_trevis@hotmail.com>
2018-05-13 20:37:04 -0700

    halfway complete

--- js/main.js
+++ js/main.js
@@ -1,131 +1,130 @@
-// ----- Classes ------
+// ----- Generators ------
 
-// Javascript doesn't have pure classes, it has prototypal inheritance.
-// ES6 has a classes feature to address this.
+// A function that we can enter and exit multiple times. Perfect for iterating though any data structure that we want.
 
-// Object constructor
-// function Car(options) {
-// 	this.title = options.title;
-// };
 
-// adding a function to the object constructor
-// Car.prototype.drive = function() {
-// 	return "vroom";
+// The forOf loop is used for iterating through arrays of data
+// const colors = [ 'red', 'blue', 'green' ];
+
+// for (let color of colors) { // iterates through each element in the colors array
 // }
 
-// creating a new instance of the object
-// const car = new Car({ title: 'Focus' });
+// console.log(colors);
+// const numbers = [ 1, 4, 6 ];
 
-// car.drive(); // returns 'vroom'
-// car // returns Car { title: "Focus" }
-
-
-// REFACTORING WITH CLASSES
-// In ES5, we would have to do the following to create the inheritance:
-// function Car(options) {
-// 	this.title = options.title;
-// };
-
-// Car.prototype.drive = function() {
-// 	return "vroom";
-// };
-
-// function Toyota(options) {
-// 	Car.call(this, options);
-// 	this.color = options.color
-// };
-
-// Toyota.prototype = Object.create(Car.prototype);
-// Toyota.prototype.constructor = Toyota;
-
-// Toyota.prototype.honk = function() {
-// 	return 'beep';
+// let total = 0;
+// for (let number of numbers) {
+// 	total += number;
 // }
 
-// const toyota = new Toyota({ color: 'red', title: 'Daily Driver' })
-// toyota; // returns { title: 'Daily Driver', color: 'red' }
-// toyota.drive(); // returns 'vroom'
-// toyota.honk(); // returns 'beep'
+// console.log(total); // returns 11
 
 
-// REFACTOR IN ES6 with classes. The benefit is that we can bypass setting up constructor function, worrying about the prototype, inheritance, etc.
-// Just doing this gives me a class object of Car:
-// class Car {
-// 	constructor(options) {
-// 		this.title = options.title
-// 	}
-// 	// Destructured:
-// 	// constructor({ title }) {
-// 	// 	this.title = title;
-// 	// }
-// 	// To add a method inside of a class, we use the enhanced object literal syntax
-// 	drive() {
-// 		return 'vroom';
-// 	}
+// An asterisk turns the function into a generator. The star can be after function or before the name of the function, i.e. function* numbers OR function *numbers, both are fine.
+// function* numbers() {
+// 	yield; // The yield keyword is used in generators
 // }
 
-// const car = new Car(); // This gives us a new instance of the Car object
-// const car = new Car({ title: 'Toyota' }); // This gives us a new instance with the title property included
-// car;
-// car.drive(); // returns 'vroom'
+// const gen = numbers();
+// gen.next(); // returns false
+// gen.next(); // returns true
 
 
-// EXTENDING BEHAVIOR WITH CLASSES
+// Let's look at an example:
+// function* shopping() {
+// 	// stuff on the sidewalk
+// 	// walking down the sidewalk
+// 	// go into the store with cash
+// 	const stuffFromStore = yield 'cash';
 
-// class Car {
-// 	constructor({ title }) {
-// 		this.title = title;
-// 	}
+// 	// What if we wanted to stop at the dry cleaners on the way home from the grocery store? Now we're walking to the laundromat.
+// 	const cleanClothes = yield 'laundry';
 
-// 	drive() {
-// 		return 'vroom';
-// 	}
+// 	// walking back home
+// 	return [ stuffFromStore, cleanClothes ];
 // }
 
-// class Toyota extends Car { // extends Car ensures that the Toyota class inherites the methods of Car
-// 	// constructor is the only method that will be called automatically when we create a new instance of Toyota
-// 	constructor(options) { // Usually, instead of destructuring the constructor, we take in the entire object and pass it along to super, and do any assignment we have to do in our local constructor, as well.
-// 		super(options); // Think of it as car.constructor
-// 		this.color = options.color;
-// 	}
+// stuff in the store
+// declaring gen doesn't actually generate anything
+// const gen = shopping();
+// gen.next(); // leaving our house
+// // walked into the store
+// // walking up and down the aisles
+// // purchase our stuff
+// gen.next('groceries'); // leaving the store with groceries
 
-// 	honk() {
-// 		return 'beep';
-// 	}
+// gen.next('clean clothes');
+
+// So, we can call yield multiple times, unlike return which breaks us out of the function. However, we need a gen.next statement for every yield in our generator.
+
+
+// HOW DO GENERATORS WORK? TYING BACK INTO THE FOROF LOOP
+
+// function* colors() {
+// 	yield 'red';
+// 	yield 'blue';
+// 	yield 'green';
 // }
 
-// const toyota = new Toyota({ color: 'red', title: "Daily Driver" });
-// toyota; // returns {'color':'red'}
-// toyota.honk(); // returns 'beep'
+// const gen = colors();
+// gen.next(); // returns {'value':'red', 'done':false}
+// gen.next(); // returns {'value':'blue', 'done':false}
+// gen.next(); // returns {'value':'green', 'done':false}
+// gen.next(); // returns {'done':true} i.e. no more yields
 
+// Generators work perfectly with for of loops
 
-// You're a game developer tasked with setting up some basic classes for a new game you are working on. Create a class called 'Monster'. In the constructor, you'll need to do some basic set up for Monster whenever they are created.
-// Initialize the Monster's health to 100
-// The constructor will be called with an 'options' object that has a 'name' property. Assign the 'name' to the Monster.
-
-// class Monster {
-// 	constructor(options) {
-// 		this.health = 100;
-// 		this.name = options.name;
-// 	}
+// function* colors() {
+// 	yield 'red';
+// 	yield 'blue';
+// 	yield 'green';
 // }
 
-// const fireUnicorn = new Monster({ name: 'HeartFire' });
-
-
-// Now that you have a monster created, create a subclass of the Monster called Snake.
-// The Snake should have a 'bite' method. The only argument to this method is another instance of Snake.
-// The instance of Snake that is passed in should have it's health deducted by 10.
-
-// class Snake extends Monster {
-// 	constructor(options) {
-// 		super(options);
-// 	}
-
-// 	bite(Snake) {
-// 		Snake.health = Snake.health - 10;
-// 	}
+// const myColors = [];
+// for (let color of colors()) {
+// 	myColors.push(color);
 // }
+
+// myColors; // returns [ 'red', 'blue', 'green']
+
+
+// Let's look at a practical example where we only want to iterate through the employees in this object, and not the size or department:
+const engineeringTeam = {
+	size: 3,
+	department: 'Engineering',
+	lead: 'Jill',
+	manager: 'Alex',
+	engineer: 'Dave'
+};
+
+// Regular function:
+// function employees() {
+// 	const names = [];
+// 	names.push(engineeringTeam.lead, engineeringTeam.manager, engineeringTeam.engineer)
+// 	return names;
+// }
+
+// Generator function:
+
+const names = [];
+
+function* TeamIterator(team) {
+	yield team.lead;
+	yield team.manager;
+	yield team.engineer;
+}
+
+// Generators are great because we can use it on multiple teams, and just pass each team into TeamIterator as an argument
+for (let name of TeamIterator(engineeringTeam)) {
+	names.push(name);
+}
+
+
+
+
+
+
+
 
 
 
